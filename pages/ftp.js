bb75bf356f65d73935fc552f9848b275bda78c63
Mustak Ahmed <musmak224@gmail.com>
2020-03-25 14:54:59 -0400

    cleaned up richards part

--- pages/ftp.js
+++ pages/ftp.js
@@ -55,8 +55,6 @@ export default function ftp() {
         text="Step 2: If you are asked for a username, try any username. If you are asked for a password, try any password."
       />
     }
-    onOpen={() => console.log('open')}
-    onClose={() => console.log('close')}
   >
      <SimpleListItem text="You should notice that the log in failed. It wants a valid password and username which we dont know. " />
   
@@ -78,15 +76,10 @@ export default function ftp() {
         text="Step 4: Try to connect to the FTP again"
       />
     }
-    onOpen={() => console.log('open')}
-    onClose={() => console.log('close')}
   >
      <SimpleListItem text=" This can be done using the linux command ftp -p <ip address> " />
   
      </CollapsibleList>
-
-
-
     
     <CollapsibleList
       handle={
@@ -95,8 +88,6 @@ export default function ftp() {
         text="Step 5: This time use ''anonymous'' or ''ftp'' as username, and leave the password empty"
       />
     }
-    onOpen={() => console.log('open')}
-    onClose={() => console.log('close')}
   >
      <SimpleListItem text=" You should have successfully loged in" />
        
@@ -109,8 +100,6 @@ export default function ftp() {
         text="Step 6: Look at all the files in the server"
       />
     }
-    onOpen={() => console.log('open')}
-    onClose={() => console.log('close')}
   >
      <SimpleListItem text=" This can be done using the ''ls'' command " />
        
@@ -124,8 +113,6 @@ export default function ftp() {
         text="Step 7: Once you have the files listed, look for an suspicious files. Attempt to get the file from the server"
       />
     }
-    onOpen={() => console.log('open')}
-    onClose={() => console.log('close')}
   >
      <SimpleListItem text=" This can be done using the ''get <filename>'' command " />
        
@@ -139,16 +126,12 @@ export default function ftp() {
         text="Step 8: Once you have gotten the file, use the ''exit'' command and view the file on your local computer"
       />
     }
-    onOpen={() => console.log('open')}
-    onClose={() => console.log('close')}
   >
      <SimpleListItem text=" check the basic commands tab to how to open the file on your computer " />
 
      <SimpleListItem text=" (the user would enter the code the found here)" />   
     
-     </CollapsibleList>
-
-    
+     </CollapsibleList> 
 
 
     </List>
