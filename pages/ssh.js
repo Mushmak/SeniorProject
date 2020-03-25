ea60c5ad02302231115d8bcc9b65f089f502ae48
Mustak Ahmed <musmak224@gmail.com>
2020-03-25 19:01:49 -0400

    changed meta icons

--- pages/ssh.js
+++ pages/ssh.js
@@ -1,13 +1,30 @@
 import Link from 'next/link'
-
+import Link from 'next/link'
+import {Card, CardPrimaryAction} from '@rmwc/card';
+import {Typography} from '@rmwc/typography';
+import {List, CollapsibleList,SimpleListItem} from '@rmwc/list';  
+import {Icon, metaIcon } from '@rmwc/icon';
 
 export default function ssh() {
     return(
-      <div>
-
-            <a> Hello from puzzle3  </a>
-
+      <>
+      <Card> 
+        <CardPrimaryAction>
+          <div style={{ padding: '1rem' }}>
+            <Typography use="headline5" tag="div">
+              SSH
+            </Typography>
+            <Typography use="body1" tag="p" theme="textSecondaryOnBackground">
+              Secure Shell or SSH for short is a software package that enables secure system administration and file transer over insecure networks. It is used nearly in 
+              every datacenter and large enterprise. 
+            </Typography>
       </div>
+      </CardPrimaryAction>
+    </Card>
+
+      </>
+      
+      
       
     )    
     
